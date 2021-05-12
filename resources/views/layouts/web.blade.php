a6d5efc6795cbe76a150dbfd5c2567776c631bef
Gulshat Taganova <gulshataganowa02@gmail.com>
2021-05-12 18:10:28 +0500

    new commit

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -12,19 +12,8 @@
 </head>
 <body>
 @yield('flash-message')
-<nav class="navbar navbar-light flex-flow bg-light">
-  <div class="container">
-    <a class="navbar-brand" href="{{ route('index') }}"><img src="{{ asset('images/logo.svg') }}" alt="logo"></a>
-    <ul class="navbar-nav ml-auto d-none d-sm-flex">
-      <li class="nav-item {{ request()->is('*projects*') ? 'active':'' }}"><a class="nav-link" href="{{ route('projects') }}">@lang('main.projects')</a></li>
-      <li class="nav-item {{ request()->is('*about*') ? 'active':'' }}"><a class="nav-link" href="{{ route('about') }}">@lang('main.about_us')</a></li>
-      {{--        <li class="nav-item {{ request()->is('*contact*') ? 'active':'' }}"><a class="nav-link" href="{{ route('contact.index') }}">@lang('main.contact_us')</a></li>--}}
-    </ul>
-    <ul class="navbar-nav ml-auto d-flex d-sm-none">
-      <li class="nav-item"><span class="nav-link" onclick="openNav()"><i data-icon="menu"></i></span></li>
-    </ul>
-  </div>
-</nav>
+
+@yield('navbar')
 
 <main class="container">
   @yield('content')
@@ -86,7 +75,7 @@
 <script src="{{ mix('js/app.js') }}"></script>
 <script>
     function openNav() {
-        document.getElementById("menu").style.width = "100%";
+        document.getElementById("menu").style.width = "75%";
     }
 
     function closeNav() {
